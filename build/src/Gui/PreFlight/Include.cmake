6c65094ef1f62bbfa0271f6bf472d13a44f72f47
Peter F Bradshaw <pfb@exadios.com>
2016-08-26 18:00:57 +0800

    Build: Started *Engine libs for namespace reasons.

--- build/src/Gui/PreFlight/Include.cmake
+++ build/src/Gui/PreFlight/Include.cmake
@@ -1,5 +1,9 @@
+set(XCSOARTHUNK_BIN_DIR ${YCSoar_BINARY_DIR}/build/src/XCSoarThunk/${T})
+set(XCSOAR_BIN_DIR ${YCSoar_BINARY_DIR}/build/submodule/xcsoar-exp/${T})
 add_executable(preflight-${T} ${PREFLIGHT}/main.cpp
                               ${PREFLIGHT}/MainWindow.cpp
                               ${PREFLIGHT}/TaskWizard.cpp
                               ${PREFLIGHT}/PreFlight.qrc)
-target_link_libraries(preflight-${T} Qt5::Widgets Qt5::Qml)
+target_link_libraries(preflight-${T} Qt5::Widgets Qt5::Qml
+                      -L${XCSOARTHUNK_BIN_DIR} xcsoarthunk-${T}
+                      -L${XCSOAR_BIN_DIR} XCSoarMain-${T})
