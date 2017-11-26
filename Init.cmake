a2848b503e6ad2a9f1b16771a48df7896e5baf9a
Peter F Bradshaw <pfb@exadios.com>
2017-11-27 02:45:40 +0800

    Build: Use gcc 4.9.

--- Init.cmake
+++ Init.cmake
@@ -1,1 +1,7 @@
+# Force CMake to use gcc 4.9.
+set(CMAKE_AR /usr/bin/gcc-ar-4.9 CACHE FILEPATH "Path to a program." FORCE)
+set(CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH "CXX compiler" FORCE)
+set(CMAKE_C_COMPILER /usr/bin/gcc-4.9 CACHE FILEPATH "C compiler" FORCE)
+set(CMAKE_NM usr/bin/gcc-nm-4.9 CACHE FILEPATH "Path to a program." FORCE)
+set(CMAKE_RANLIB /usr/bin/gcc-ranlib-4.9 CACHE FILEPATH "Path to a program." FORCE)
 set(CMAKE_CXX_FLAGS "-Wno-error=unused-parameter -Wall -Wextra -Wwrite-strings -Wcast-qual -Wpointer-arith -Wsign-compare -Wundef -Wmissing-declarations -Wredundant-decls -Wmissing-noreturn -Wno-unused-parameter -Wno-missing-field-initializers  -Wcast-align -Werror -fno-exceptions -fno-rtti -std=gnu++11 -fno-threadsafe-statics -fmerge-all-constants -fvisibility=hidden -fpic -funwind-tables -finput-charset=utf-8" CACHE STRING "Flags used by the compiler during all build types" FORCE)
