d48c44d53e688154fc431581c4b60be0eb0a4f7b
Peter F Bradshaw <pfb@exadios.com>
2018-07-20 18:12:59 +0800

    Build: Added Flight.

---
+++ build/src/Gui/Flight/Include.cmake
@@ -0,0 +1,23 @@
+set(XCSOARTHUNK_BIN_DIR ${YCSoar_BINARY_DIR}/build/src/XCSoarThunk/${T})
+set(XCSOARSUBST_BIN_DIR ${YCSoar_BINARY_DIR}/build/src/XCSoarSubst/${T})
+set(XCSOAR_BIN_DIR ${YCSoar_BINARY_DIR}/build/submodule/xcsoar-exp/${T})
+set(XCSOAR_SRC_DIR ${YCSoar_SOURCE_DIR}/submodule/xcsoar-exp/src)
+set(XCSOARTHUNK_SRC_DIR ${YCSoar_SOURCE_DIR}/src/XCSoarThunk)
+set(XCSOARSUBST_SRC_DIR ${YCSoar_SOURCE_DIR}/src/XCSoarSubst)
+add_executable(flight-${T} ${FLIGHT}/main.cpp)
+target_link_libraries(flight-${T} Qt5::Widgets Qt5::Qml
+                      -L${XCSOARSUBST_BIN_DIR} xcsoarmain-subst-${T}
+                      -L${XCSOARTHUNK_BIN_DIR} xcsoarthunk-${T}
+                      -L${XCSOAR_BIN_DIR} XCSoarMain-${T}
+                      -L${XCSOAR_BIN_DIR} Io-${T}
+                      -L${XCSOAR_BIN_DIR} Terrain-${T}
+                      -L${XCSOAR_BIN_DIR} Thread-${T}
+                      -L${XCSOAR_BIN_DIR} WaypointEngine-${T}
+                      -L${XCSOAR_BIN_DIR} TaskEngine-${T}
+                      -L${XCSOAR_BIN_DIR} Time-${T})
+target_include_directories(flight-${T} PRIVATE ${XCSOARSUBST_SRC_DIR}
+                                               ${XCSOARTHUNK_SRC_DIR}
+                                               ${XCSOAR_SRC_DIR})
+include_directories(BEFORE ${XCSOARTHUNK_SRC_DIR}
+                           ${XCSOARSUBST_SRC_DIR})
+include_directories(${XCSOAR_SRC_DIR})
--- src/Gui/Flight/main.cpp
+++ src/Gui/Flight/main.cpp
@@ -2,11 +2,11 @@
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
+  {
+  QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-    return app.exec();
-}
+  return app.exec();
+  }
